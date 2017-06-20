5e682ffbdc3123f6ae6de2a8900274e0d7eac1ae
bvcitro <citro09@gmail.com>
2017-06-20 09:54:29 -0500

    Update enlargeimg.js

--- enlargeimg.js
+++ enlargeimg.js
@@ -1,5 +1,6 @@
+var imagez;
 $(document).ready(function(){ // ensures this doesnt run until the image is loaded
-  var imagez = document.getElementById("divmyImg"); // gets the div holding the image
+  imagez = document.getElementById("divmyImg"); // gets the div holding the image
     if (imagez===null) document.getElementById('divone').style.display = "none";// if there is no image, does not display button  
   imagez = imagez.firstElementChild.src; // get the element holding the image url
   imagez = imagez.replace(/&/g, "XXXXXXXXXX");// gets rid of '&' within the url so the url can be transferred within another url

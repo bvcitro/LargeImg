280de09e789396821e8de46275e5e508531c80ac
bvcitro <citro09@gmail.com>
2017-06-20 10:08:24 -0500

    Update enlargeimg.js

--- enlargeimg.js
+++ enlargeimg.js
@@ -1,13 +1,16 @@
-var imagez;
-$(document).ready(function(){ // ensures this doesnt run until the image is loaded
-  imagez = document.getElementById("divmyImg"); // gets the div holding the image
-  if (imagez===null) {
-      alert("Image Null :(");
-      document.getElementById('divone').style.display = "none";// if there is no image, does not display button  
-  }else{// if images null end
-       alert(imagez.src);
-       imagez = imagez.firstElementChild.src; // get the element holding the image url
-       imagez = imagez.replace(/&/g, "XXXXXXXXXX");// gets rid of '&' within the url so the url can be transferred within another url
-       document.getElementById("buttonz").href = "http://citrowske.com/Training_Photos/PhotoHost.html?url="+imagez; //puts the modified image url within the host url and links it to the button
-   }// if else imagez null end
- })
+
+function getit(){
+  var imagez;
+  $(document).ready(function(){ // ensures this doesnt run until the image is loaded
+   imagez = document.getElementById("divmyImg"); // gets the div holding the image
+   if (imagez===null) {
+        alert("Image Null :(");
+       document.getElementById('divone').style.display = "none";// if there is no image, does not display button  
+   }else{// if images null end
+        alert(imagez.src);
+        imagez = imagez.firstElementChild.src; // get the element holding the image url
+        imagez = imagez.replace(/&/g, "XXXXXXXXXX");// gets rid of '&' within the url so the url can be transferred within another url
+        document.getElementById("buttonz").href = "http://citrowske.com/Training_Photos/PhotoHost.html?url="+imagez; //puts the modified image url within the host url and links it to the button
+    }// if else imagez null end
+   })
+}// function end

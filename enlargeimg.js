50163ac86c840dd04124e8fa0170d2c666048224
bvcitro <citro09@gmail.com>
2017-06-20 10:23:17 -0500

    Update enlargeimg.js

--- enlargeimg.js
+++ enlargeimg.js
@@ -6,7 +6,7 @@ $(document).ready(function(){ // ensures this doesnt run until the image is load
       document.getElementById('divone').style.display = "none";// if there is no image, does not display button  
   }else{// if images null end
        imagez = imagez.firstElementChild.src; // get the element holding the image url
-       alert(imagez.src);
+       //alert(imagez.src);
        imagez = imagez.replace(/&/g, "XXXXXXXXXX");// gets rid of '&' within the url so the url can be transferred within another url
        document.getElementById("buttonz").href = "http://citrowske.com/Training_Photos/PhotoHost.html?url="+imagez; //puts the modified image url within the host url and links it to the button
    }// if else imagez null end
